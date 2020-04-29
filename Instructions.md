5e83b2e59f11d2b938703ece57942719ceca5303
Dom <dom@pilifter.com>
2020-04-29 22:35:07 +0100

    Initial commit

---
+++ Instructions.md
@@ -0,0 +1,13 @@
+Please follow the instructions and ask questions if you are unsure. Points *will* be deducted for missing/incorrect executions!
+
+Please ensure your code is uploaded to GitHub and that is is functioning.
+
+1. You make an API call of which the response can be found in the file response.js. Write code that will return the number of leaders in the response.
+
+2. Create a function that will take in a leader and return the title, first name, last name and country of the leader in one line.
+
+3. Create an HTML document. The document should display the list of leaders found in the response. Each leader should have their own <div>, and should show the title, first and last name, and country.
+
+4. Import moment.js. Create a function that will take in a string in 'yyyy-mm-dd' format, and return the number of days since that date.
+
+5. Augment the function in (2) so that it includes the phrase "Number of days in office" and displays the number of days. Ensure this information is displayed for each leader.
---
+++ response.js
@@ -0,0 +1,44 @@
+data ={leaders:[
+    {
+        FirstName: 'Sanna',
+        LastName: 'Marin',
+        OfficialTitle: 'Prime Minister',
+        Country: 'Finland',
+        DateAppointed: '2019-12-10'
+    },
+    {
+        FirstName: 'Sophie',
+        LastName: 'Wilmes',
+        OfficialTitle: 'Prime Minister',
+        Country: 'Belgium',
+        DateAppointed: '2019-10-27'
+    },
+    {
+        FirstName: 'Paula-Mae',
+        LastName: 'Weekes',
+        OfficialTitle: 'President',
+        Country: 'Belgium',
+        DateAppointed: '2018-08-19'
+    },
+    {
+        FirstName: 'Jacinda',
+        LastName: 'Arden',
+        OfficialTitle: 'Prime Minister',
+        Country: 'New Zealand',
+        DateAppointed: '2017-11-30'
+    },
+    {
+        FirstName: 'Tsai',
+        LastName: 'Ing-wen',
+        OfficialTitle: 'President',
+        Country: 'Taiwan',
+        DateAppointed: '2016-05-20'
+    },
+    {
+        FirstName: 'Angela',
+        LastName: 'Merkel',
+        OfficialTitle: 'Chancellor',
+        Country: 'Germany',
+        DateAppointed: '2005-11-22'
+    }
+]}
